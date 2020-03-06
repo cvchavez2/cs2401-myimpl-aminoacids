c97dac78e8c6083e41356dfcb32ee4a0fb4a4c8b
Dan DeBlasio <dan@dandeblasio.com>
2020-03-06 15:21:46 -0700

    Adding Linked List shell

---
+++ src/AminoAcidLL.java
@@ -0,0 +1,99 @@
+class AminoAcidLL{
+  char aminoAcid;
+  String[] codons;
+  int[] counts;
+  AminoAcidLL next;
+
+  AminoAcidLL(){
+
+  }
+
+
+  /********************************************************************************************/
+  /* Creates a new node, with a given amino acid/codon 
+   * pair and increments the codon counter for that codon.
+   * NOTE: Does not check for repeats!! */
+  AminoAcidLL(String inCodon){
+  
+  }
+
+
+  /********************************************************************************************/
+  /* Given a codon string, finds the index for that codon and increments the count */
+  private void incrementCodon(String inCodon){
+  
+  }
+
+
+  /********************************************************************************************/
+  /* Recursive method that increments the count for a specific codon:
+   * If it should be at this node, increments it and stops, 
+   * if not passes the task to the next node. 
+   * If there is no next node, add a new node to the list that would contain the codon. 
+   */
+  private void addCodon(String inCodon){
+  
+  }
+
+
+  /********************************************************************************************/
+  /* Shortcut to find the total number of instances of this amino acid */
+  private int totalCount(){
+    return 0;
+  }
+
+  /********************************************************************************************/
+  /* Recursive method that finds the differences in **Amino Acid** counts. 
+   * the list *must* be sorted to use this method */
+  public int aminoAcidCompare(AminoAcidLL inList){
+    return 0;
+  }
+
+  /********************************************************************************************/
+  /* helper method for finding the list difference on two matching nodes */
+  private int codonDiff(AminoAcidLL inList){
+    return 0;
+  }
+
+
+  /********************************************************************************************/
+  /* Same ad above, but counts the codon usage differences
+   * Must be sorted. */
+  public int codonCompare(AminoAcidLL inList){
+    return 0;
+  }
+
+
+  /********************************************************************************************/
+  /* Recursively returns the total list of amino acids in the order that they are in in the linked list. */
+  public char[] aminoAcidList(){
+    return new char[]{};
+  }
+
+  /********************************************************************************************/
+  /* Recursively returns the total counts of amino acids in the order that they are in in the linked list. */
+  public int[] aminoAcidCounts(){
+    return new int[]{};
+  }
+
+
+  /********************************************************************************************/
+  /* recursively determines if a linked list is sorted or not */
+  public boolean isSorted(){
+    return false;
+  }
+
+
+  /********************************************************************************************/
+  /* Static method for generating a linked list from an RNA sequence */
+  public static AminoAcidLL createFromRNASequence(String inSequence){
+    return null;
+  }
+
+
+  /********************************************************************************************/
+  /* sorts a list by amino acid character*/
+  public static AminoAcidLL sort(AminoAcidLL inList){
+    return null;
+  }
+}
